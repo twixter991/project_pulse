2dbd837a6205c72b57447a27cb41f0ce5d909b9e
Artem <misterslain@gmail.com>
2021-01-13 23:01:23 +0300

    first page

--- gulpfile.js
+++ gulpfile.js
@@ -15,21 +15,20 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src('src/sass/*.+(sass|scss)')
+    return gulp.src('src/sass/**/*.+(sass|scss)')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
             prefix: "",
             suffix: ".min",
           }))
-        .pipe(autoprefixer({
-            cascade: false}))
+        .pipe(autoprefixer({}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 })
 
 gulp.task('watch', function() {
-    gulp.watch('src/sass/*.+(sass|scss)', gulp.parallel("styles"));
+    gulp.watch('src/sass/**/*.+(sass|scss)', gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
